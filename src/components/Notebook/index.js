a11eeeccf33792686635a09d918e87f904f73564
paulsancer <paulsancer.io@gmail.com>
2019-12-10 22:37:37 -0600

    api integration /api/victims

--- src/components/Notebook/index.js
+++ src/components/Notebook/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import dummyPosts from 'victimsData';
 import TargetCard from './TargetCard';
+import Loader from 'components/utils/Loader';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,7 +18,23 @@ const useStyles = makeStyles(theme => ({
 const Notebook = () => {
   const classes = useStyles();
 
-  const [victims, setVictims] = useState(dummyPosts);
+  const [isLoading, setIsLoading] = useState(true);
+  const [victims, setVictims] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setIsLoading(true);
+      try {
+        const resp = await axios.get('/api/victims');
+        setVictims(resp.data);
+      } catch (error) {}
+      setIsLoading(false);
+    };
+
+    setTimeout(() => {
+      fetch();
+    }, 500);
+  }, []);
 
   const setUpdatingVictim = (victimId, isLoading = true) => {
     const newVictims = [...victims];
@@ -26,13 +43,49 @@ const Notebook = () => {
     setVictims(newVictims);
   };
 
+  const putVictim = async victim => {
+    try {
+      const { data } = await axios.put(`/api/victims/${victim.id}`, victim);
+      return data;
+    } catch (error) {
+      setIsLoading(false);
+      return null;
+    }
+  };
+
+  const deleteVictim = async victim => {
+    try {
+      const resp = await axios.delete(`/api/victims/${victim.id}`, victim);
+      console.log(resp);
+      return true;
+    } catch (error) {
+      setIsLoading(false);
+      return false;
+    }
+  };
+
   const handleVote = victimId => {
     setUpdatingVictim(victimId, true);
-    setTimeout(() => {
-      const newVictims = [...victims];
-      const victim = newVictims.find(v => v.id === victimId);
+    setTimeout(async () => {
+      const victim = victims.find(v => v.id === victimId);
       victim.votes += 1;
-      victim.isLoading = false;
+      victim.isLoading = undefined;
+      const newVictim = await putVictim(victim);
+      const newVictims = victims.map(vic =>
+        vic.id === newVictim ? newVictim : vic
+      );
+      setVictims(newVictims);
+    }, 1000);
+  };
+
+  const handleDelete = victimId => {
+    setUpdatingVictim(victimId, true);
+    setTimeout(async () => {
+      const victim = victims.find(v => v.id === victimId);
+      const isDeleted = await deleteVictim(victim);
+      const newVictims = !isDeleted
+        ? [...victims]
+        : victims.filter(vic => vic.id !== victim.id);
       setVictims(newVictims);
     }, 1000);
   };
@@ -40,16 +93,20 @@ const Notebook = () => {
   return (
     <div className={classes.root}>
       <Grid container spacing={3} direction="row">
-        {victims &&
+        {isLoading ? (
+          <Loader />
+        ) : (
           victims.map(victim => (
             <Grid key={victim.id} item xs={12} sm={6} md={4} lg={3}>
               <TargetCard
                 victim={victim}
                 isLoading={victim.isLoading}
                 handleVote={handleVote}
+                handleDelete={handleDelete}
               />
             </Grid>
-          ))}
+          ))
+        )}
       </Grid>
     </div>
   );
